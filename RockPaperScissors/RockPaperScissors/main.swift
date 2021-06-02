03308d6514ccf57a0b5f43efd2d4d75f816078f4
KimWanKi <seoulkwk95@gmail.com>
2021-06-02 10:02:45 +0900

    refactor : 조건문을 메소드로 변경.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 struct RockPaperScissorsGameConsole {
-    private var isGameOver: Bool?
+    private var isGameOver: Bool = false
     private var computerRandomNumber: Int?
 
     private enum RockPaperScissors: Int {
@@ -16,30 +16,43 @@ struct RockPaperScissorsGameConsole {
         case paper
     }
     
-    func checkGameOver(inputNumber: Int) -> Bool {
+    private func isUserWin(_ userHand: RockPaperScissors, _ computerHand: RockPaperScissors) -> Bool {
+        if (userHand == .scissors && computerHand == .paper) || (userHand == .paper && computerHand == .rock) || (userHand == .rock && computerHand == .scissors) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    private func checkGameOver(inputNumber: Int) -> Bool {
         if inputNumber == 0 {
             print("게임 종료")
             return true
         }
         return false
     }
+    
     private func requestUserInput() -> Int {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        guard let inputString = readLine(), let inputNumber = Int(inputString), 0 <= inputNumber && inputNumber <= 3 else {
+        guard let inputString = readLine(),
+              let inputNumber = Int(inputString),
+              0 <= inputNumber && inputNumber <= 3 else {
             print("잘못된 입력입니다. 다시 시도해주세요.")
             return requestUserInput()
         }
         return inputNumber
     }
-    private mutating func generateRandomNumber() {
-        computerRandomNumber = Int.random(in: 1...3)
+    
+    private mutating func generateRandomNumber() -> Int {
+        return Int.random(in: 1...3)
     }
-    private mutating func compareWithUserInput(userNumber: Int) {
-        guard let unwrappedNumber = computerRandomNumber else { return }
-        guard let userHand = RockPaperScissors(rawValue: userNumber), let computerHand = RockPaperScissors(rawValue: unwrappedNumber) else { return }
+    
+    private mutating func printResultAfterCompareComputerNumber(with userNumber: Int) {
+        let computerNumber = generateRandomNumber()
+        guard let userHand = RockPaperScissors(rawValue: userNumber), let computerHand = RockPaperScissors(rawValue: computerNumber) else { return }
         if userHand == computerHand {
             print("비겼습니다!")
-        } else if (userHand == .scissors && computerHand == .paper) || (userHand == .paper && computerHand == .rock) || (userHand == .rock && computerHand == .scissors) {
+        } else if isUserWin(userHand, computerHand) {
             print("이겼습니다!")
             isGameOver = true
         } else {
@@ -47,13 +60,13 @@ struct RockPaperScissorsGameConsole {
             isGameOver = true
         }
     }
+    
     mutating func gameStart() {
         isGameOver = false
         while isGameOver == false {
             let userNumber = requestUserInput()
             isGameOver = checkGameOver(inputNumber: userNumber)
-            generateRandomNumber()
-            compareWithUserInput(userNumber: userNumber)
+            printResultAfterCompareComputerNumber(with: userNumber)
         }
     }
 }
